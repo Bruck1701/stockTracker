85fb9ef3f88209e8833dc388022e27f1d506e814
Bruck1701 <kummel@gmail.com>
2020-07-06 09:48:54 +0200

    fix: order of routes and change about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,21 @@ Rails.application.routes.draw do
   
   
   resources :user_invs, only: [:create, :destroy]
+  devise_for :users
+  root to: "pages#index"
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_investment', to: 'investments#search'
+  get 'friends', to: 'users#friends'
+  get 'search_friend', to: 'users#search'
 
   resources :friendships, only: [:create, :destroy]
-
   resources :users, only: [:show]
 
-  devise_for :users
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
 
-  get 'friends', to: 'users#friends'
   
-  get 'my_portfolio', to: 'users#my_portfolio'
-
-  get 'search_investment', to: 'investments#search'
-  
-  get 'search_friend', to: 'users#search'
-
-  root to: "pages#index"
 
 
 end
